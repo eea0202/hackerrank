0cb31702a3ca0ee02e46d88ceb4838c602c7c7bc
Emerald <emeraldangus@gmail.com>
2020-08-17 19:16:57 -0700

    start of IfElse

---
+++ python/IfElse.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+#https://www.hackerrank.com/challenges/py-if-else/problem?h_r=next-challenge&h_v=zen
+#didwithfamily
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
