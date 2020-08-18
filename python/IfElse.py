3d9fb0a4ef8337076f6da9feacbbd0ae61a72aae
Emerald <emeraldangus@gmail.com>
2020-08-17 19:56:40 -0700

    IfElse is now working

--- python/IfElse.py
+++ python/IfElse.py
@@ -2,6 +2,12 @@
 #https://www.hackerrank.com/challenges/py-if-else/problem?h_r=next-challenge&h_v=zen
 #didwithfamily
 
+#If n is odd, print Weird
+#If n is even and in the inclusive range of 2 to 5, print Not Weird
+#If n is even and in the inclusive range of 6 to 20, print Weird
+#If n is even and greater than 20, print Not Weird
+
+
 import math
 import os
 import random
@@ -12,3 +18,12 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
+    if (n % 2 != 0):
+        print("Weird")
+    else:
+        if (n >= 2 and n <= 5):
+            print("Not Weird")
+        elif (n >= 6 and n <= 20):
+                print("Weird")
+        elif (n >20):
+            print("Not Weird")
